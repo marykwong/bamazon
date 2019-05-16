1852361d85c633e6b43685c272a2febc8274791c
Mary <marykwong27@gmail.com>
2019-05-16 00:55:25 -0700

    initial files

--- bamazon.sql
+++ bamazon.sql
@@ -3,11 +3,12 @@ CREATE database bamazon_DB;
 
 USE bamazon_DB;
 
-CREATE TABLE auctions(
-  id INT NOT NULL AUTO_INCREMENT,
-  item_name VARCHAR(100) NOT NULL,
-  category VARCHAR(45) NOT NULL,
-  starting_bid INT default 0,
-  highest_bid INT default 0,
-  PRIMARY KEY (id)
+CREATE TABLE products(
+  sku INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price DECIMAL(10,2) default 0,
+  stock_quantity INT default 0,
+  PRIMARY KEY (sku)
 );
+
