87ab479075b00d8f5a20f3a14c47beb277d17e06
Mary <marykwong27@gmail.com>
2019-05-16 01:02:51 -0700

    initial files

--- bamazon.sql
+++ bamazon.sql
@@ -12,3 +12,32 @@ CREATE TABLE products(
   PRIMARY KEY (sku)
 );
 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Pencil", "Office Supplies", 2.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Spatula", "Kitchen", 2.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Microwave", "Kitchen", 29.00, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Pillow", "Bedding", 10.00, 3);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Popcorn", "Snacks", 2.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Sweater", "Clothing", 25.00, 9);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Earrings", "Accessories", 22.00, 8);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bacon", "Food", 7.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Pens", "Office Supplies", 2.00, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Salt", "Food", 1.00, 30);
